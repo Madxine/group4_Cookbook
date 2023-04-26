6980cedcdb1b0893f0c28736ed4f6b160a088aeb
Yosra gmati <yosra@air-de-yosra.speedport.ip>
2023-04-26 11:12:42 +0200

    firststep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { client } from "./Client";
+import { useState, useEffect } from "react";
 function App() {
+  const [recipes, setRecipes] = useState();
+
+  useEffect(() => {
+    client
+      .getEntries()
+      .then((data) => setRecipes(data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(recipes);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> {recipes?.items[0].fields.description}</h1>
+      <img src={recipes?.items[0].fields.wafflePicture[0].fields.file.url} />
     </div>
   );
 }
---
+++ src/Client.js
@@ -0,0 +1,6 @@
+import * as contentful from "contentful";
+
+export const client = contentful.createClient({
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  space: process.env.REACT_APP_SPACE_ID,
+});
