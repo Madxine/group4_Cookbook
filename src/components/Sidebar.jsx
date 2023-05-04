cc4ead2c3fbf9fb5523baa9f596ccda0e2ef65d8
Stephan Ullmann <stephan.ullmann@posteo.de>
2023-05-04 20:16:04 +0200

    added spoonacular call on hitting nutrition tab

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,10 @@ import { ContentfulContext } from "../context/ContentfulContext";
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import "../css/Sidebar.css";
+import { NutritionContext } from "../context/NutritionContext";
 export default function Sidebar() {
   const { recipes } = useContext(ContentfulContext);
+  const { setRecipeIndex } = useContext(NutritionContext);
 
   return (
     <ul className="sidebar">
@@ -13,6 +15,7 @@ export default function Sidebar() {
             className="sidebar__item"
             key={recipe.sys.id}
             to={`/${ind}/ingredients`}
+            onClick={() => setRecipeIndex(ind)}
           >
             <h2>{recipe.fields.waffles}</h2>
             <img
